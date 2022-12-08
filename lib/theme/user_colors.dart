299de72bab6c3e798693d301703bd33ba2a28627
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2022-12-08 20:04:14 +0500

    Adds user colors

---
+++ lib/theme/user_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class UserColors {
+  static const blue = Color(0xFF222976);
+  static const red = Color(0xFFE05252);
+  static const green = Color(0xFF127961);
+}
